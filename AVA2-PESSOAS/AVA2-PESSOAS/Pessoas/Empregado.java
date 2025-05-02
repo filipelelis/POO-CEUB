573f0fead5bad62336a0a59656d454e6a4c5178b
Filipe Soares Lelis <fslelis@gmail.com>
2025-05-02 18:05:35 -0300

    AVA2-3-POO-QUI- FILIPE SOARES LELIS

---
+++ AVA2-PESSOAS/AVA2-PESSOAS/Pessoas/Empregado.java
@@ -0,0 +1,23 @@
+public class Empregado extends Pessoas {
+    private final String cargoEmpregado;
+    private final float salarioEmpregado;
+    
+public class Empregado (
+    private final String nomePessoa;
+    private final String endereçoPessoa;
+    private final String telefonePessoa;
+    private final String cargoEmpregado;
+    private final float salarioEmpregado;
+){
+    super(nomePessoa, endereçoPessoa, telefonePessoa,)
+    this.cargoEmpregado = cargoEmpregado;
+    this.salarioEmpregado = salarioEmpregado;
+}
+public float calcularSalario() {
+    return calcularSalario;                              
+}
+public void registrarPonto() {
+    return registrarPonto;                              
+}
+}
+
